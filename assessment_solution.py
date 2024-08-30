80d61e329cbe0e722e1102a693f450cd30b8f8cb
Kevin Langat <kevin.langat@student.moringaschool.com>
2024-08-30 16:13:34 +0300

    Adds update on numbers and text

--- assessment_solution.py
+++ assessment_solution.py
@@ -5,3 +5,14 @@ def add_numbers(num1, num2):
     """
     return num1 + num2
 
+def is_even(number):
+    """
+    Returns True if the number is even, otherwise False.
+    """
+    return number % 2 == 0
+
+def reverse_string(text):
+    """
+    Returns the reversed version of the input string.
+    """
+    return text[::-1]
